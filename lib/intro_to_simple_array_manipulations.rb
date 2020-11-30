ea2023b353f6c2eb4bbbaf6b3e53c0e385644207
Jeffrey Platt <jsplatt@gmail.com>
2020-11-30 17:03:06 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -5,12 +5,12 @@ def using_concat(a,b)
   a.concat[(b)]
 end
 
-#array = [1]
-#element = 2
+array = [1]
+element = 2
 
-#def using_insert(array)
-  #array.insert(element)
-#end  
+def using_insert(array)
+  array.insert(element)
+end  
 
 
 
